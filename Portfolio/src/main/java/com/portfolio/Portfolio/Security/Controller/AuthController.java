690ba55814d969b8450ac7a4e158dae9905547ba
Lucas Robles <robles.lucasm@gmail.com>
2022-10-27 19:40:09 -0300

    deploy

--- Portfolio/src/main/java/com/portfolio/Portfolio/Security/Controller/AuthController.java
+++ Portfolio/src/main/java/com/portfolio/Portfolio/Security/Controller/AuthController.java
@@ -37,6 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "https://roblesportfoliofrontend.web.app")
+
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
